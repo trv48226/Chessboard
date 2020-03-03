3c03e50c2531fb1701a0b4419119403f3b69bd6f
trv48226 <trv48226@bethel.edu>
2020-03-03 17:19:59 -0600

    python flip
    Used python to flip the colors. Had to use a round about way to get the background colors though. Also ajax only is working with chrome

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,5 +10,15 @@ def chess_board():
     return render_template('chess.html')
 
 
+@app.route('/flip', methods=["POST"])
+def flip_colors():
+    first_color = str(json.loads(request.data).get('color1'))
+    second_color = str(json.loads(request.data).get('color2'))
+    my_dict = {'color1': first_color, 'color2': second_color}
+    return my_dict
+
+
+
 if __name__ == '__main__':
     app.run()
+
--- static/chess.js
+++ static/chess.js
@@ -23,3 +23,29 @@ function buttonFlip(){
     }
     flip = !flip;
 }
+
+function myAjax(){
+let color1 = document.getElementsByClassName("even")[0].style.backgroundColor;
+let color2 = document.getElementsByClassName("odd")[0].style.backgroundColor;
+$.ajax({
+    url : "/flip",
+    type: "POST",
+    data: JSON.stringify({
+        'color1': color1,
+        'color2': color2
+    }),
+    contentType: 'application/json;charset=UTF-8',
+    success: function(response)
+    {
+        console.log(response)
+        for(i = 0; i < table.length/2; i++){
+            document.getElementsByClassName("even")[i].style.backgroundColor = color2;
+            document.getElementsByClassName("odd")[i].style.backgroundColor = color1;
+        }
+    },
+    error: function (response)
+    {
+
+    }
+});
+}
